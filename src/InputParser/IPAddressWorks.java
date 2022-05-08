ce9896387c1794eef02b074efb66da2f8828c1ad
Frane Karinja <frane.karinja@gmail.com>
2022-05-08 11:51:45 +0200

    Ufff opet sam se zaboravio commitati. * Napravio glavni meni * Napravio pod-meni za pinganje * Napravio dvije metode da provjeri da li je lista IP adresa prazna * napravio izlist ip adresa

--- src/InputParser/IPAddressWorks.java
+++ src/InputParser/IPAddressWorks.java
@@ -4,7 +4,6 @@ import com.sun.jdi.InvalidLineNumberException;
 
 import java.io.IOException;
 import java.net.InetAddress;
-import java.net.NetworkInterface;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 
@@ -13,6 +12,10 @@ public class IPAddressWorks {
     private static ArrayList<InetAddress> inetAddressList = new ArrayList<InetAddress>();
 
     public static void initialParser(String address){
+        System.out.println("[*] Starting IP address enumeration ...");
+        //System.out.println("[*] Replying only with IP live addresses. "); //iako je neuredno treba biti feedback da li je dostupno
+        //jer može proći puno vremena ako veći dio mreže nije dostupan.
+
         if(address.contains("-")){
             dashSeparatorIPAddressList(address);
         } else {
@@ -20,8 +23,25 @@ public class IPAddressWorks {
         }
     }
 
-    //Ako je separator
+
     private static void dashSeparatorIPAddressList(String stringAddress){
+        //Odvajam zadnji dio ili gornju granicu
+        String[] stringAddressSep = stringAddress.split("-");
+        int end = Integer.parseInt(stringAddressSep[1]);
+
+        //odvajam donju granicu
+        // "." je predefinirani class character, pa da bi ga split uzeo kao char, moram ga unescape-ati
+        String[] initAdd = stringAddressSep[0].split("\\.");
+        int start = Integer.parseInt(initAdd[3]);
+
+        //Spajam na string bez zadnjeg dijela
+        String base = initAdd[0].toString() + "." + initAdd[1].toString()  + "." + initAdd[2].toString()  + ".";
+
+
+        for(int i = start; i <= end; i++){
+            String singleIP = base + i;
+            singleIPAdressIsReacable(singleIP);
+        }
 
 
     }
@@ -29,30 +49,44 @@ public class IPAddressWorks {
     private static void singleIPAdressIsReacable(String stringAddress){
 
         //System.out.println("[*]");    // Test da li ulazi ovdje.
+
         try {
             // u ovom dijelu samo pulim listu, nakon toga ide u klasu koja će otvoriti
+
             InetAddress address = InetAddress.getByName(stringAddress);
             if (address.isReachable(50)){
-                System.out.println("[*] IP Address: " + address.getHostAddress() + " IS reacheable!");
-                System.out.println("[*] Starting Enumeration ...");
+                System.out.println("=> : " + address.getHostAddress() + " IS reacheable!");
+                System.out.println("Adding to the list of potential targets.");
+
                 inetAddressList.add(address);
             } else {
-                System.out.println("[!] IP address: " + address.getHostAddress() + " is NOT reacheable!");
+                System.out.println("=>  " + address.getHostAddress() + " no reply.");
             }
 
 
-
+        //default exceptions koje sam "hendlao"
         } catch (UnknownHostException e) {
-            throw new RuntimeException(e);
+            System.out.println("Parsing the IP address returned an error. Please check your input!");
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            System.out.println("Something went wrong trying to contact the IP address you provided.");
+            System.out.println("Are you connected to any networks?");
         }
 
 
     }
 
+    public static boolean isLAddressistEampty(){
+        return inetAddressList.isEmpty();
+    }
 
 
+    public static void listTargets(){
+        System.out.println("[*] Potential targets in the list:");
+        for(InetAddress addr : inetAddressList){
+            System.out.print("IP address: ");
+            System.out.println(addr.toString());
+        }
+    }
 
 
 }
--- src/UserInterface/MainApp.java
+++ src/UserInterface/MainApp.java
@@ -2,23 +2,70 @@ package UserInterface;
 
 import InputParser.IPAddressWorks;
 
+import java.net.NetworkInterface;
 import java.util.Scanner;
+
 
 public class MainApp {
     // Privatni scanner jer ću ga koristiti više pute za ostale korisničke kontrole, za sada izgleda da mi neće biti potreban System.in osim u glavnom sučelju.
-    private static Scanner sc = new Scanner(System.in);
+     private static Scanner sc = new Scanner(System.in);
+     private static Boolean quit = false;
 
     public static void main(String[] args) {
-        //Main menu
-        System.out.println("============================================================");
-//        System.out.println("IP address ie. => 192.168.1.1");
-//        System.out.println("IP address range ie. => 192.168.1.1-255");
-//        System.out.println("Please enter IP address or IP address range.");
-
-        //String address = sc.nextLine();
-        String address = "192.168.8.1-255";
-        IPAddressWorks.initialParser(address);
+       //call Main menu
+       menu();
 
 
     }
+    private static void menu(){
+
+        setCa
+        System.out.println("\n============================================================");
+        System.out.println("Simple network scanner");
+        System.out.println("============================================================\n");
+
+
+        if (IPAddressWorks.isLAddressistEampty()){
+            System.out.println("[*] Current target list is EMPTY!");
+        } else {
+            IPAddressWorks.listTargets();
+        }
+        System.out.println("------------------------------------------------------------");
+        System.out.println("[*] To add enter targets menu, enter \"ping\" or \"P\"");
+        //System.out.println("[*] To add enter targets menu, enter \"ping\"");
+        System.out.println("[*] To quit enter \"quit\" or  \"Q\"");
+
+        System.out.println();
+        while (quit == false){
+            String input = sc.nextLine();
+            input = input.trim().toLowerCase();
+            if(input.equals("ping") || input.equals("p")){
+                getAddressInput();
+            } else if(input.equals("quit") || input.equals("q")){
+                System.out.println("[*]");
+                quit = true;
+            }
+        }
+    }
+
+    private static void getAddressInput(){
+        System.out.println("------------------------------------------------------------");
+        System.out.println("IP address ie. => 192.168.1.1");
+        System.out.println("IP address range ie. => 192.168.1.1-255");
+        System.out.println("Please enter IP address or IP address range.");
+
+        String address = sc.nextLine();
+
+        IPAddressWorks.initialParser(address);
+        cleanScreen();
+        menu();
+    }
+
+    private static void cleanScreen(){
+        for( int i = 0; i <= 20; i++){
+            System.out.println("\n");
+        }
+    }
+
+
 }
